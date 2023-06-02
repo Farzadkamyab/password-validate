1214122f59ebb7d730b28b770e5cf140f691eb41
FarzadKamyab <farzadkamyabofficial@gmail.com>
2023-06-02 09:29:49 -0400

    write operation

---
+++ math_operation/main.py
@@ -0,0 +1,14 @@
+from math_operation import addition,subtraction,multiplication, division
+
+x = 10
+y = 11
+
+result_addition = addition.add(x, y)
+result_subtraction = subtraction.subtract(x, y)
+result_multiplication = multiplication.multiply(x, y)
+result_division = division.divide(x, y)
+
+print("Addition:", result_addition)
+print("Subtraction:", result_subtraction)
+print("Multiplication:", result_multiplication)
+print("Division:", result_division)
