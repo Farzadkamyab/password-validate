daad3f8714aafa3c29e82b2d69667a62f6da05b8
FarzadKamyab <farzadkamyabofficial@gmail.com>
2023-06-02 08:24:50 -0400

    adding if to code

--- CW9.py
+++ CW9.py
@@ -4,7 +4,11 @@ def passwd_validate(password:str):
     password = str(password)
 
     pattern = re.compile("^(?=.*[a-z])(?=.*[a-z])(?=.*/d)[A-Zz-z\d@$!#%*?&]{8,}$")
-    return bool(pattern.match(password))
+    
+    if re.match(pattern, password):
+        print('valid')
+    else:
+        print('invalid')
 
 if __name__ == '__main__':
     passwd = "Farzad021"
